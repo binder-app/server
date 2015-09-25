57b018d826b201d333adb66905f8ba68cff81ec8
Mitchell Hentges <mitch9654@gmail.com>
2015-09-25 10:40:54 -0700

    Testing deployment server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Foo.swag');
 });
 
 var server = app.listen(8000, function () {
