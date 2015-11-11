5dba711720dddd3bc5e73f5a09c09639b3a62ae0
Chris Foster <chris.james.foster@gmail.com>
2015-11-11 11:49:56 -0800

    Add a suggestions route

--- server/routes/suggestions.js
+++ server/routes/suggestions.js
@@ -3,8 +3,24 @@
  * Return user profiles to the user
  */
 
+var r = require('../r');
+
 module.exports.get = get;
 
-function get(req, res) {
-  res.send([ ]);
+function get(req, res, next) {
+  r.table('Profile')
+  .filter(r.row('id').ne(req.user))
+  .filter(function(profile) {
+    return r.table('Vote')
+    .filter({ 
+      from : req.user,
+      to   : profile('id')
+    })
+    .count()
+    .eq(0);
+  })
+  .then(users => {
+    res.send(users);
+  })
+  .catch(next);
 };
