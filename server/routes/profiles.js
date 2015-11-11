3ff3ef1a96a69a7e1cf8e30a22d8ae4eba007c25
Chris Foster <chris.james.foster@gmail.com>
2015-11-11 11:49:47 -0800

    Add a profiles route

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -3,8 +3,27 @@
  * Allow a user to update their profile
  */
 
+var r = require('../r');
+
 module.exports.put = put;
 
-function put(req, res) {
-  res.send([ ]);
+function put(req, res, next) {
+  if (req.params.id != req.user) {
+    var err = { error : 'You can only update your own profile' };
+    return res.status(403).send(err);
+  }
+  var profile = {
+    id      : req.user,
+    name    : req.body.name || '',
+    bio     : req.body.bio || '',
+    courses : req.body.courses || []
+  };
+  r.table('Profile')
+  .insert(profile, {
+    conflict : 'replace'
+  })
+  .then(() => {
+    res.send(profile);
+  })
+  .catch(next);
 };
